d9df8573e9a7759507b2905a8cfb1d3ccd05cd5f
Cleiton <cleitonjosedf@gmail.com>
2022-06-16 21:39:02 -0300

    MOD5-LAV

--- vnet-usa01.tf
+++ vnet-usa01.tf
@@ -1,22 +1,22 @@
-resource "azurerm_virtual_network" "ResVnet-eastus2" {
+resource "azurerm_virtual_network" "ResVnet-eastus" {
   name                = var.resource_vnet_name_vnet_usa01
   location            = var.az_vnet_Location_eastus
   resource_group_name = azurerm_resource_group.ResGroup.name
   address_space       = var.address_vnet_usa01
   tags                = merge(var.tags_vnet, { treinamento = "terraform" }, )
 }
-resource "azurerm_subnet" "ResSubnet-eastus2" {
+resource "azurerm_subnet" "ResSubnet-eastus" {
   count                = length(var.resource_subnet_name_vnet_usa01)
   name                 = var.resource_subnet_name_vnet_usa01[count.index]
   resource_group_name  = azurerm_resource_group.ResGroup.name
-  virtual_network_name = azurerm_virtual_network.ResVnet-eastus2.name
+  virtual_network_name = azurerm_virtual_network.ResVnet-eastus.name
   address_prefixes     = [var.address_prefix_subnet_vnet_usa01[count.index]]
 }
 
 resource "azurerm_subnet" "hub-gateway-subnet" {
   name                 = "GatewaySubnet"
   resource_group_name  = azurerm_resource_group.ResGroup.name
-  virtual_network_name = azurerm_virtual_network.ResVnet-eastus2.name
+  virtual_network_name = azurerm_virtual_network.ResVnet-eastus.name
   address_prefixes     = ["10.1.255.224/27"]
 }
 resource "azurerm_public_ip" "pipVNGW" {
